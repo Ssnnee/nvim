a2cad5262b02fe7bca27132bd1016623f74b8269
Nandi Samuel <nandisamuelsne@gmail.com>
2025-07-25 15:23:42 +0100

    feat: add terminal toggle
    The idea here is to try to use neovim buit-in terminal instead of tmux
    ar at leat to use it less oftn

--- lua/sne/init.lua
+++ lua/sne/init.lua
@@ -1,6 +1,7 @@
 require("sne.set")
 require("sne.remap")
 require("sne.lazy_init")
+require("sne.terminal")
 require("sne.present").setup()
 -- require("sne.autoclose").setup()
 
@@ -21,9 +22,44 @@ local SneGroup = augroup('Sne', {})
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
-function R(name)
-    require("plenary.reload").reload_module(name)
+local state = {
+  floating = {
+    buf = -1,
+    win = -1,
+  }
+}
+
+
+local function create_terminal_window(opts)
+  opts = opts or {}
+
+  -- Create a buffer
+  local buf = nil
+  if vim.api.nvim_buf_is_valid(opts.buf) then
+    buf = opts.buf
+  else
+    buf = vim.api.nvim_create_buf(false, true)
+  end
+
+  -- Create the floating window
+  local win = vim.api.nvim_open_win(buf, true)
+
+  return { buf = buf, win = win }
 end
+
+local toggle_terminal = function()
+  if not vim.api.nvim_win_is_valid(state.floating.win) then
+    state.floating = create_terminal_window { buf = state.floating.buf }
+    if vim.bo[state.floating.buf].buftype ~= "terminal" then
+      vim.cmd.terminal()
+    end
+  else
+    vim.api.nvim_win_hide(state.floating.win)
+  end
+end
+
+vim.api.nvim_create_user_command("Toggleterminal", toggle_terminal, {})
+
 
 vim.filetype.add({
     extension = {
@@ -46,6 +82,11 @@ autocmd({"BufWritePre"}, {
     group = SneGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
+})
+
+autocmd("TermOpen", {
+    pattern = "*",
+    command = "startinsert",
 })
 
 autocmd('LspAttach', {
